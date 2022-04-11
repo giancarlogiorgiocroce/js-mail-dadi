abcb3743966634d574fbc38355e11577a8f5010d
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-04-11 16:34:28 +0200

    Fine del js per l'esercizio del dado (manca il risultato in html)

--- dadi/script.js
+++ dadi/script.js
@@ -6,14 +6,26 @@
 
 
 const estrattore = document.querySelector('button');
-console.log(estrattore);
+const dadoUno = document.querySelector('#dado-uno');
+const dadoDue = document.querySelector('#dado-due');
 
 estrattore.addEventListener('click', function(){
 
     let dadoGiocatore = Math.floor(Math.random() * 6 + 1);
     console.log('questo è il dado giocatore',dadoGiocatore);
+    // dadoGiocatore.append(dadoUno);
 
     let dadoBot = Math.floor(Math.random() * 6 + 1);
     console.log('questo è il dado del bot', dadoBot);
+    // dadoBot.append(dadoDue);
+
+
+    if(dadoGiocatore > dadoBot){
+        alert("Il giocatore ha vinto!");
+    } else if (dadoGiocatore < dadoBot){
+        alert("Il bot ha vinto!");
+    } else {
+        alert ("Wow, pareggio!");
+    };
 
 });
